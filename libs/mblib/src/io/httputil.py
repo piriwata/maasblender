d79f8051ed32dea804da1e3094dcb89c231a3bbf
Yukihisa Fujita <22886319+yukifuji@users.noreply.github.com>
2024-03-27 17:29:50 +0900

    add common library

---
+++ libs/mblib/src/io/httputil.py
@@ -0,0 +1,109 @@
+# SPDX-FileCopyrightText: 2024 TOYOTA MOTOR CORPORATION
+# SPDX-License-Identifier: Apache-2.0
+import dataclasses
+import logging
+import urllib.parse
+from pprint import pformat
+
+import aiohttp
+import fastapi
+import pydantic
+from pydantic_settings import BaseSettings
+
+logger = logging.getLogger(__name__)
+LoggerType = logging.Logger | logging.LoggerAdapter
+
+
+async def error_log(response: aiohttp.ClientResponse, logger_: LoggerType = logger):
+    request = response.request_info
+    message = f"{request.method} {request.url} [status={response.status}]"
+    try:
+        content = await response.json()
+        match response.status:
+            case fastapi.status.HTTP_422_UNPROCESSABLE_ENTITY:
+                text = pformat(content)
+            case _:
+                text = pformat(content, width=200)
+        logger_.error("%s:\n%s", message, text)
+    except:
+        try:
+            text = await response.text()
+            logger_.error("%s: %s", message, text)
+        except:  # ignore error
+            logger_.error("%s: cannot read response", message)
+    return message
+
+
+def check_limit_file_size(data: bytes, *, limit=0):
+    """if data size over limit, raise fastapi.HTTPException"""
+    if limit:
+        file_size = len(data)
+        if file_size > limit:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_400_BAD_REQUEST,
+                detail=f"file size limit over: {file_size} > {limit}",
+            )
+
+
+async def check_response(response: aiohttp.ClientResponse, *, limit=0):
+    """if error response, raise exception and output error log"""
+    if not response.ok:
+        message = await error_log(response)
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=message,
+        )
+    elif limit:
+        check_limit_file_size(await response.read(), limit=limit)
+
+
+def check_upload_filename(upload_file: fastapi.UploadFile):
+    filename = urllib.parse.unquote(upload_file.filename)
+    if filename != upload_file.filename:
+        logger.warning("upload file with urlencoded filename: %s, raw: %s", filename, upload_file.filename)
+    return filename
+
+
+class FileManagerConfig(BaseSettings, frozen=True):
+    """environment variable"""
+    FILE_SIZE_LIMIT: int = 0  # file size limit for each input file
+
+
+@dataclasses.dataclass(frozen=True)
+class FileManager:
+    env: FileManagerConfig = dataclasses.field(default_factory=FileManagerConfig)
+    table: dict[str, bytes] = dataclasses.field(default_factory=dict)
+
+    @property
+    def limit(self):
+        return self.env.FILE_SIZE_LIMIT
+
+    def put(self, upload_file: fastapi.UploadFile):
+        data = upload_file.file.read()
+        check_limit_file_size(data, limit=self.limit)
+        filename = check_upload_filename(upload_file)
+        self.table[filename] = data
+
+    async def _fetch(self, session: aiohttp.ClientSession, url: str):
+        async with session.get(url) as resp:
+            await check_response(resp, limit=self.limit)
+            data = await resp.read()
+            return resp.content_disposition.filename, data
+
+    async def pop(
+            self, session: aiohttp.ClientSession,
+            *, url: str | pydantic.AnyHttpUrl = None, filename: str = None,
+    ) -> tuple[str, bytes]:
+        if url:
+            if not isinstance(url, str):
+                url = str(url)
+            return await self._fetch(session, url)
+        elif filename:
+            data = self.table.pop(filename)
+            return filename, data
+        else:
+            raise fastapi.HTTPException(status_code=fastapi.status.HTTP_400_BAD_REQUEST,
+                                        detail=f"exists directory in GBFS zip file")
+
+    def clear(self):
+        self.table.clear()
---
+++ libs/mblib/src/jschema/spec.py
@@ -0,0 +1,102 @@
+# SPDX-FileCopyrightText: 2024 TOYOTA MOTOR CORPORATION
+# SPDX-License-Identifier: Apache-2.0
+import dataclasses
+import typing
+from functools import cached_property
+
+from pydantic import BaseModel, Field, AnyHttpUrl
+from pydantic.json_schema import JsonSchemaValue
+
+TxRx = typing.Literal["Tx", "Rx"]
+
+
+class FeatureDefinition(BaseModel):
+    declared: list[str] | None = Field(None, title="declare to support features")
+    required: list[str] | None = Field(None, title="declare and require to support features for other side")
+
+
+class EventDefinition(BaseModel):
+    dir: TxRx = Field(title="incoming or outgoing")
+    schema_: JsonSchemaValue | None = Field(
+        None,
+        title="json schema of event",
+        description="if null, schema validation not supported",
+        alias="schema",
+    )
+    feature: FeatureDefinition | None = Field(
+        None,
+        title="supported features of event",
+        description="if null, features validation not supported",
+    )
+
+    @staticmethod
+    def inspect(dir_: TxRx, event_class: typing.Type[BaseModel]):
+        if hasattr(event_class, "feature"):
+            feature = event_class.feature
+        else:
+            feature = None
+        return EventDefinition(dir=dir_, schema=event_class.model_json_schema(), feature=feature)
+
+
+class SpecificationResponse(BaseModel):
+    version: AnyHttpUrl = Field(
+        title="URI to identify corresponding API version",
+        description="URI indicates event schema on base of version",
+    )
+    events: dict[str, EventDefinition] | None = Field(
+        None,
+        title="definition for supported event by type",
+        description="if null, validation not supported",
+    )
+
+
+# class type of StepEvent or TriggeredEvent
+EventClassType = typing.Union[typing.Type[BaseModel], ...] | typing.Type[BaseModel] | None
+
+
+@dataclasses.dataclass(frozen=True)
+class EventSpecificationBuilder:
+    events: dict[str, EventDefinition] = dataclasses.field(default_factory=dict)
+    step: dataclasses.InitVar[EventClassType | None] = None
+    triggered: dataclasses.InitVar[EventClassType | None] = None
+
+    def __post_init__(self, step: EventClassType = None, triggered: EventClassType = None):
+        # set event specification of step API response
+        self._set_events("Tx", step)
+        # set event specification of triggered API request
+        self._set_events("Rx", triggered)
+
+    @staticmethod
+    def _extract_element_type_of_union(event_class: EventClassType):
+        if event_class:
+            event_classes = getattr(event_class, "__args__", None)
+            if event_classes:
+                yield from event_classes
+            else:  # if only one event type
+                yield event_class
+
+    def _set_events(self, dir_: TxRx, event_class: EventClassType):
+        for e in self._extract_element_type_of_union(event_class):
+            schema = e.model_json_schema()
+            event_type = schema["properties"]["eventType"]["const"]
+            self.events[event_type] = EventDefinition(dir=dir_, schema=schema)
+
+    def set_feature(self, event_type: typing.Any, *, declared: list[str] = None, required: list[str] = None) -> None:
+        """set feature definition for step or triggered event specification"""
+        feature = FeatureDefinition(declared=declared, required=required)
+        self.events[str(event_type)].feature = feature
+
+    def get_specification_response(self, *, version: str) -> SpecificationResponse:
+        """get specification response data"""
+        if self.events:
+            return SpecificationResponse(version=version, events=self.events)
+        else:
+            return SpecificationResponse(version=version)
+
+    @property
+    def schemas(self) -> dict[str, JsonSchemaValue | None]:
+        return {event_type: e.schema_ for event_type, e in self.events.items()}
+
+    @cached_property
+    def event_types_of_triggered(self) -> set[str]:
+        return set(self.events.keys())
