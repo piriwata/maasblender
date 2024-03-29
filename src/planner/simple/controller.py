a353634b5a392c3c6d4f74dfd6b288aee39f34e0
Yukihisa Fujita <22886319+yukifuji@users.noreply.github.com>
2024-03-29 05:45:11 +0000

    update simple planner

--- src/planner/simple/controller.py
+++ src/planner/simple/controller.py
@@ -19,6 +19,8 @@ from gtfs.reader import FilesReader as GtfsFiles
 from gtfs_flex.network import Network as GtfsFlexNetwork
 from gtfs_flex.reader import FilesReader as GtfsFlexFiles
 from jschema import query, response
+from mblib.io import httputil
+from mblib.io.log import init_logger
 from route_planner import Planner, DirectPathPlanner
 
 logger = logging.getLogger(__name__)
@@ -33,23 +35,7 @@ app = fastapi.FastAPI(
 
 @app.on_event("startup")
 def startup():
-    class MultilineLogFormatter(logging.Formatter):
-        def format(self, record: logging.LogRecord) -> str:
-            message = super().format(record)
-            return message.replace(
-                "\n", "\t\n"
-            )  # indicate continuation line by trailing tab
-
-    formatter = MultilineLogFormatter(env.log_format)
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logging.basicConfig(level=env.log_level, handlers=[handler])
-
-    # replace logging formatter for uvicorn
-    for handler in logging.getLogger("uvicorn").handlers:
-        handler.setFormatter(formatter)
-
-    logger.debug("configuration: %s", env.json())
+    init_logger()
 
 
 @app.exception_handler(Exception)
